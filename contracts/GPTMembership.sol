203f1fb47c6d3c505d5dd4fe0bee1202e98bec5c
Nova2419 <shivamojha2419@gmail.com>
2024-02-12 15:12:39 +0530

    Membership Contract Done

---
+++ contracts/GPTMembership.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract GPTMembership is ERC721{
+ address public owner;
+ uint256 public membershipTypes;
+ uint256 public totalMemberships;
+
+ string public MY_CONTRACT = "Shivam Ojha";
+
+ struct Membership{
+  uint256 id;
+  string name;
+  uint256 cost;
+  string date;
+ }
+
+ struct UserMembership{
+  uint256 id;
+  uint256 membershipId;
+  address addressUser;
+  uint256 cost;
+  string expireDate;
+ }
+
+ mapping(address => UserMembership) userMemberships;
+ mapping(uint256 => Membership) memberships;
+ mapping(uint256 => mapping(address => bool)) public hasBought;
+ mapping(uint256 => mapping(uint256 => address)) public membershipTaken;
+ mapping(uint256 => uint256[]) membershipsTaken;
+
+modifier onlyOwner() {
+  require(msg.sender == owner, "Only Owner can call this function!!");
+  _;
+}
+
+constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+  owner = msg.sender;
+}
+
+
+function list(string memory _name, uint256 _cost, string memory _date) public onlyOwner() {
+  membershipTypes++;
+  memberships[membershipTypes] = Membership(
+    membershipTypes,
+    _name,
+    _cost,
+    _date
+  );
+}
+
+function mint(uint256 _membershipId, address _user, string memory _expiredDate) public payable {
+  require(_membershipId != 0, "Invalid membership ID"); // More informative message
+  require(_membershipId <= membershipTypes, "Membership ID out of range");
+
+  require(msg.value >= memberships[_membershipId].cost, "Insufficient Balance!");
+  totalMemberships++;
+
+  userMemberships[_user] = UserMembership(
+    totalMemberships,
+    _membershipId,
+    _user,
+    memberships[_membershipId].cost,
+    _expiredDate
+  );
+
+  hasBought[_membershipId][msg.sender] = true;
+  membershipTaken[_membershipId][totalMemberships] = msg.sender;
+
+  membershipsTaken[_membershipId].push(totalMemberships);
+
+  _safeMint(msg.sender, totalMemberships);
+}
