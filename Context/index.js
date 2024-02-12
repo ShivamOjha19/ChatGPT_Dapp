a261a55fd62a616cda0cba5f6840299eb3207f7f
Nova2419 <shivamojha2419@gmail.com>
2024-02-12 18:20:42 +0530

    Setting Up code for frontend done

---
+++ Context/index.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState, createContext, useContext} from "react";
+import { ethers } from "ethers";
+
+//Internal Import
+import {} from '../Utils/apiFeature'
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({children}) => {
+ 
+ const DAPP_NAME = "GPT_MEMBERSHIP";
+
+ return(
+  <StateContext.Provider value={{DAPP_NAME}}>
+   {children}
+  </StateContext.Provider>
+ );
+};
+
+export const useStateContext = () => useContext(StateContext);
